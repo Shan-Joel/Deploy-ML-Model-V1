c6186d40f97ddb2fd8e262c43124eef6a16f7264
Dharmagunarathna S J - IT20210892 <it20210892@my.sliit.lk>
2023-11-15 11:40:51 +0530

    changed to getenv

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os
 # # Load environment variables from the .env file
 # load_dotenv()
 
-api_key = os.environ.get("OPENAI_KEY")
+api_key = os.getenv("OPENAI_KEY")
 
 def extract_job_details(input):
 
